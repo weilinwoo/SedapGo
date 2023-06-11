6a7bd157032395268f97a3c81fb77790bcb88f80
weilinwoo <111231431+weilinwoo@users.noreply.github.com>
2023-06-11 10:07:33 +0800

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,5 +1,5 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=FRFDS','woo', '1234');
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=FRFDS','username', 'password');
 // See the "errors" folder for details...
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
